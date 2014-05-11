67e849929e964234159f4d80131da834c1e3ca44
Laura Hernández Bethencourt <alu0100821475@ull.edu.es>
2014-05-11 22:13:29 +0100

    Cambiooos

---
+++ src/biseccion.py
@@ -0,0 +1,26 @@
+#! encoding: UTF-8
+#! /usr/bin/python 
+
+import math
+
+Cero=0.00001
+
+def f(x):
+ return math.sin(x)
+
+def biseccion(a,b,tol):
+ c=float((a+b)/2.0)
+ while f(c) != Cero and abs(b-a) > tol:
+  if f(a) * f(c) < Cero:
+   b = c;
+  else:
+   a = c;
+  c = (a+b)/2.0
+ return c
+
+print 'Calcular la raíz de sen de x'
+a = float(raw_input('Valor a del intervalo: '))
+b = float(raw_input('Valor b del intervalo: '))
+t = 0.00000000000001
+r = biseccion(a,b,t)
+print "El valor de la raíz de seno de x es: %f"%(r)
